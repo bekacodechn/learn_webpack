664e6ff3df16fb07ab51725efc0fd747dc371955
bekacodechn <bekacodechn@gmail.com>
2024-05-29 18:40:48 +0800

    处理字体,使用了ttf没效果,但资源移到了output

--- packages/course_1/webpack.config.js
+++ packages/course_1/webpack.config.js
@@ -19,6 +19,10 @@ const config = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
